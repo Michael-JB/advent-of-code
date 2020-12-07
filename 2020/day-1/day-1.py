2ce485fb343f76022a0f4e02d0e804d584f71842
Michael B <michael@ramyun.co.uk>
2020-12-07 22:26:48 +0000

    Comment, tidy

--- 2020/day-1/day-1.py
+++ 2020/day-1/day-1.py
@@ -1,4 +1,13 @@
+#!/usr/bin/env python3
+
 import argparse
+
+"""
+Part 1: Find the product of a pair    of numbers in the input such that their sum is 2020
+Part 2: Find the product of a triplet of numbers in the input such that their sum is 2020
+
+Assumption by me: the same number can be used multiple times, e.g. an input containing 1010.
+"""
 
 # Finds two numbers in `values` which sum to `target`
 def find_pair_for_sum(values, target):
@@ -17,9 +26,7 @@ def solve_part_1(values):
 def solve_part_2(values):
   for value in values:
     remainder = 2020 - value
-    filtered_values = set(values)
-    filtered_values.remove(value)
-    solution = find_pair_for_sum(filtered_values, remainder)
+    solution = find_pair_for_sum(values, remainder)
     if solution is not None:
       return value * solution[0] * solution[1]
   return None
@@ -30,18 +37,16 @@ def main():
   parser.add_argument('input', help='a text file containing the Advent of Code problem input')
   args = parser.parse_args()
 
-  # Read input into set
-  values = set()
+  # Read input into a set (fast lookup, duplicates are not a concern)
   with open(args.input) as input_file:
-    for value in input_file:
-      values.add(int(value))
+    values = set(int(value) for value in input_file.readlines())
 
-  # Solve problems
-  solution_to_part_1 = solve_part_1(values)
-  print("Solution to part 1:", solution_to_part_1)
+    # Solve problems
+    solution_to_part_1 = solve_part_1(values)
+    print("Solution to part 1:", solution_to_part_1)
 
-  solution_to_part_2 = solve_part_2(values)
-  print("Solution to part 2:", solution_to_part_2)
+    solution_to_part_2 = solve_part_2(values)
+    print("Solution to part 2:", solution_to_part_2)
 
 if __name__ == '__main__':
   main()
