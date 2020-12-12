7bd018009b237e32e07fbf27fa63fe012d2b33ea
Michael B <michael@ramyun.co.uk>
2020-12-12 21:27:34 +0000

    Update to use RegEx

--- 2020/day-2/day-2.py
+++ 2020/day-2/day-2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import re
 
 """
 Part 1:
@@ -8,24 +9,21 @@ Given an input with a 'policy' and a 'password' on each line, as follows:
   1-3 a: abcde
   1-3 b: cdefg
   2-9 c: ccccccccc
-calculate the number of valid passwords in the input, where a password is valid iff the count of the
-policy letter in the password is within the policy interval.
+calculate the number of valid passwords in the input, where a password is valid iff. the count of
+the policy letter in the password is within the policy interval.
 
 Part 2:
-Same as above, though now a password is valid iff the letter occurs at exactly one of the two locations
-indexed by the policy
+Same as above, though now a password is valid iff the letter occurs at exactly one of the two
+locations indexed by the policy
 
 Assumption: well-formed input
 """
 
-# This could be improved with RegEx, and it would be nicer to store the data in a more structured manner
-def parse_policy(policy):
-  dash_split = policy.split('-')
-  first_number = int(dash_split[0])
-  space_split = dash_split[1].split(' ')
-  second_number = int(space_split[0])
-  letter = space_split[1]
-  return first_number, second_number, letter
+policy_re = re.compile(r'^(\d+)-(\d+)\s([a-z]):\s([a-z]+)$')
+
+def parse_line(policy_string):
+  parsed_value = policy_re.match(policy_string)
+  return (int(parsed_value[1]), int(parsed_value[2]), parsed_value[3]), parsed_value[4]
 
 def validate_with_count_policy(policy, password):
   count = password.count(policy[2])
@@ -36,7 +34,7 @@ def validate_with_index_policy(policy, password):
   return extracted_letters.count(policy[2]) == 1
 
 def count_valid_passwords(values, validation_function):
-  valid_values = [(policy, password) for (policy, password) in values if validation_function(policy, password)]
+  valid_values = [(pol, pas) for pol, pas in values if validation_function(pol, pas)]
   return len(valid_values)
 
 def main():
@@ -46,14 +44,13 @@ def main():
   args = parser.parse_args()
 
   with open(args.input) as input_file:
-    values = [tuple(value.split(': ')) for value in input_file.readlines()]
-    parsed_values = [(parse_policy(value[0]), value[1].strip()) for value in values]
+    values = [parse_line(policy_string.strip()) for policy_string in input_file.readlines()]
 
     # Solve problems
-    solution_to_part_1 = count_valid_passwords(parsed_values, validate_with_count_policy)
+    solution_to_part_1 = count_valid_passwords(values, validate_with_count_policy)
     print("Solution to part 1:", solution_to_part_1)
 
-    solution_to_part_2 = count_valid_passwords(parsed_values, validate_with_index_policy)
+    solution_to_part_2 = count_valid_passwords(values, validate_with_index_policy)
     print("Solution to part 2:", solution_to_part_2)
 
 
